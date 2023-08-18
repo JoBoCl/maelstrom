2c3d79785344a1867cc08ceb91e4c54af5a55d7a
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2023-08-18 15:48:29 +0200

    Part 3 distributed working

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,16 +1,19 @@
+use async_recursion::async_recursion;
 use async_trait::async_trait;
 use maelstrom::protocol::Message;
-use maelstrom::{done, Node, Result, Runtime};
-use serde::Serialize;
-use std::collections::HashSet;
+use maelstrom::{done, Node, Runtime};
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
 use std::sync::{Arc, Mutex};
+use tokio_context::context::Context;
 
-pub(crate) fn main() -> Result<()> {
+pub(crate) fn main() -> maelstrom::Result<()> {
+    color_eyre::install()?;
     let state = Arc::new(Mutex::new(State::default()));
     Runtime::init(try_main(state))
 }
 
-async fn try_main(state: Arc<Mutex<State>>) -> Result<()> {
+async fn try_main(state: Arc<Mutex<State>>) -> maelstrom::Result<()> {
     let handler = Arc::new(Handler::new(state));
     Runtime::new().with_handler(handler).run().await
 }
@@ -18,14 +21,25 @@ async fn try_main(state: Arc<Mutex<State>>) -> Result<()> {
 #[derive(Default)]
 struct State {
     seen: HashSet<i64>,
+    neighbours: Vec<String>,
 }
 
 impl State {
-    fn add(&mut self, v: i64) {
-        self.seen.insert(v);
+    fn add(&mut self, v: i64) -> bool {
+        self.seen.insert(v)
     }
     fn all(&self) -> HashSet<i64> {
         self.seen.clone()
+    }
+    fn set_neighbours(&mut self, neighbours: Vec<String>) -> color_eyre::Result<()> {
+        if self.neighbours.is_empty() {
+            self.neighbours = neighbours;
+            return Ok(());
+        }
+        Err(color_eyre::eyre::eyre!("Tried to set neighbours twice"))
+    }
+    fn neighbours(&self) -> &Vec<String> {
+        &self.neighbours
     }
 }
 
@@ -34,55 +48,91 @@ struct Handler {
     state: Arc<Mutex<State>>,
 }
 
+#[async_recursion]
+async fn call_with_retry(
+    n: String,
+    value: i64,
+    runtime: Runtime,
+) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (context, _handle) = Context::with_timeout(std::time::Duration::from_millis(100));
+    let mut call = match runtime
+        .rpc(n.clone(), BroadcastProtocol::Broadcast { message: value })
+        .await
+    {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    match call.done_with(context).await {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            log::warn! {"Broadcast {value} to {n} failed with error {e:?}, retrying..."};
+            call_with_retry(n, value, runtime).await
+        }
+    }
+}
+
 impl Handler {
     pub fn new(state: Arc<Mutex<State>>) -> Self {
         Handler { state }
     }
 
-    fn to_response(&self, request: BroadcastRequest) -> std::io::Result<BroadcastResponse> {
+    fn to_response(
+        &self,
+        request: BroadcastProtocol,
+        runtime: &Runtime,
+    ) -> color_eyre::Result<BroadcastProtocol> {
         match request {
-            BroadcastRequest::Broadcast(v) => {
-                self.state.lock().unwrap().add(v);
-                Ok(BroadcastResponse::BroadcastOk {})
+            BroadcastProtocol::Broadcast { message: value } => {
+                if self.state.lock().unwrap().add(value) {
+                    log::warn! {"New value {value} seen, broadcasting to neighbours."};
+                    for n in self.state.lock().unwrap().neighbours() {
+                        log::warn! {"Broadcasting {value} to neighbour {n}."};
+                        runtime.spawn(call_with_retry(n.clone(), value, runtime.clone()));
+                    }
+                } else {
+                    log::info! {"Already seen {value}, not sending it again."};
+                }
+                Ok(BroadcastProtocol::BroadcastOk {})
             }
-            BroadcastRequest::Read() => Ok(BroadcastResponse::ReadOk {
+            BroadcastProtocol::Read {} => Ok(BroadcastProtocol::ReadOk {
                 messages: self.state.lock().unwrap().all(),
             }),
-            BroadcastRequest::Topology(_) => Ok(BroadcastResponse::TopologyOk {}),
+            BroadcastProtocol::Topology { topology } => {
+                if let Some(neighbours) = topology.get(runtime.node_id()) {
+                    return match self
+                        .state
+                        .lock()
+                        .unwrap()
+                        .set_neighbours(neighbours.clone())
+                    {
+                        Ok(()) => Ok(BroadcastProtocol::TopologyOk {}),
+                        Err(e) => Err(e),
+                    };
+                }
+                Err(
+                    color_eyre::eyre::eyre! {"Could not find a list of neighbours for node {}", runtime.node_id()},
+                )
+            }
+            v => Err(color_eyre::eyre::eyre! {"Unexpected input: {v:?}"}),
         }
     }
 
-    fn to_request(&self, message: &Message) -> std::io::Result<BroadcastRequest> {
-        match message.get_type() {
-            "broadcast" => {
-                let value = message
-                    .body
-                    .extra
-                    .get("message")
-                    .and_then(|v| v.as_i64())
-                    .ok_or_else(|| {
-                        std::io::Error::new(
-                            std::io::ErrorKind::NotFound,
-                            "message not found for broadcast",
-                        )
-                    });
-                Ok(BroadcastRequest::Broadcast(value?))
-            }
-            "read" => Ok(BroadcastRequest::Read()),
-            "topology" => Ok(BroadcastRequest::Topology(Neighbours::default())),
-            _ => Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                format! {"Did not recognise input: {}", message.get_type()},
-            )),
+    fn to_request(&self, message: &Message) -> color_eyre::Result<BroadcastProtocol> {
+        match serde_json::from_value(message.body.raw()) {
+            Ok(v) => Ok(v),
+            Err(e) => Err(color_eyre::eyre::eyre! {e}),
         }
     }
 }
 
 #[async_trait]
 impl Node for Handler {
-    async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
+    async fn process(&self, runtime: Runtime, req: Message) -> maelstrom::Result<()> {
+        log::error! {"Receved request: {req:?}"};
         if let Ok(request) = self.to_request(&req) {
-            if let Ok(response) = self.to_response(request) {
+            if let Ok(response) = self.to_response(request, &runtime) {
                 return runtime.reply(req, response).await;
             }
         }
@@ -91,20 +141,23 @@ impl Node for Handler {
     }
 }
 
-#[derive(Clone, Debug)]
-enum BroadcastRequest {
-    Broadcast(i64),
-    Read(),
-    Topology(Neighbours),
-}
-
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
-enum BroadcastResponse {
+enum BroadcastProtocol {
+    Broadcast {
+        message: i64,
+    },
+    BroadcastBatch {
+        message: Vec<i64>,
+    },
+    Read {},
+    Topology {
+        topology: HashMap<String, Vec<String>>,
+    },
     BroadcastOk {},
-    ReadOk { messages: HashSet<i64> },
+    ReadOk {
+        messages: HashSet<i64>,
+    },
     TopologyOk {},
+    BroadcastBatchOk {},
 }
-
-#[derive(Default, Clone, Debug)]
-struct Neighbours {}
